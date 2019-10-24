9057e97880d9f386d871c0d7962cf051512b615f
aaron97 <aaron.glez997@gmail.com>
2019-10-23 23:07:42 -0500

    Un cambio en el index se agrego un encabezado

---
+++ Demostracion/index.php
@@ -0,0 +1,291 @@
+
+
+
+    <title>Facultad Artes</title>
+
+
+    
+    <!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/styles.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+    
+
+<!------links------------>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<!----------------->
+
+
+
+<head>
+     <?php
+        include 'nav2.html';
+     ?>
+</head>
+
+
+ <div style="height:30px"></div>
+
+    <div id="navegacion" class="wrapper center">
+        <ul class="tabs">
+            <li class="tab"><a class="active" href="#least">Galería</a></li>
+            <li class="tab"><a href="#videos">Vídeos</a></li>
+        </ul>
+    </div>
+
+<body>
+   
+    
+<div class="container">
+   <section class="main row">
+    
+   <article class="col-md-3">
+        
+<h1 id="fh5co-logo"><a href="index.html"><img src="icon/LogoArtes.png"  class="img-responsive" alt="Facultad De Artes"></a></h1>
+        
+        
+    <!---------------barra nav------------->
+    <ul class="nav"> 
+              <li>
+                <a class="active" href="#Conversatorios" data-toggle="tab">Conversatorios</a>
+              </li>
+              <li> 
+                <a  href="#Charlas" data-toggle="tab">Charlas</a>
+              </li>
+              <li>
+                <a c href="#Mesas"data-toggle="tab">Mesas de dialogo</a>
+              </li>
+              <li> 
+                <a  href="#Coloquios"data-toggle="tab">Coloquios</a>
+              </li>
+                <li> 
+                <a  href="#Congresos"data-toggle="tab">Congresos</a>
+              </li>
+                <li> 
+                <a  href="#Simposios"data-toggle="tab">Simposios</a>
+              </li>
+                <li> 
+                <a  href="#Cineclub"data-toggle="tab">Cineclub</a>
+              </li>
+                <li> 
+                <a  href="#Festivales"data-toggle="tab">Festivales</a>
+              </li>
+                <li> 
+                <a  href="#Otros"data-toggle="tab">Otros</a>
+              </li>
+    </ul>
+    </article>
+       
+       
+       
+<!---------------contenido nav------------->   
+                <aside class="col-md-6">
+                  
+<!-----------------Conversatorio-------------------->             
+<div class="tab-content">
+<div role="tabpanel"  class="tab-pane fade" id="Conversatorios">
+    <h1> Conversatorios</h1>
+
+            <iframe width="250" height="180" src="https://www.youtube.com/embed/n_LKGnx9OMc" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+            <iframe width="250" height="180"src="https://www.youtube.com/embed/E937mlU2kd4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>  
+    
+</div>
+<!--------------------------------------->
+
+
+
+    
+    
+<!-----------------Charlas-------------------->
+<div role="tabpanel"  class="tab-pane fade" id="Charlas">
+    <h1>Charlas</h1>
+            <iframe width="250" height="180" src="https://www.youtube.com/embed/n_LKGnx9OMc" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+            <iframe width="250" height="180"src="https://www.youtube.com/embed/E937mlU2kd4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        
+            <iframe width="250" height="180" src="https://www.youtube.com/embed/n_LKGnx9OMc" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            
+            <iframe width="250" height="180" src="https://www.youtube.com/embed/n_LKGnx9OMc" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    
+    
+</div>      
+<!------------------------------------------->
+                    
+ 
+    
+    
+    
+    
+<!-----------------Mesas de dialogo-------------------->
+<div role="tabpanel"  class="tab-pane fade" id="Mesas">
+    <h1>Mesas de dialogo</h1>                   
+            <iframe width="250" height="180" src="https://www.youtube.com/embed/n_LKGnx9OMc" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    
+            <iframe width="250" height="180"src="https://www.youtube.com/embed/E937mlU2kd4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+</div>
+<!----------------------------------------->   
+            
+            
+
+    
+    
+<!-----------------Coloquios-------------------->
+<div role="tabpanel"  class="tab-pane fade" id="Coloquios">
+    <h1>Coloquios</h1>                   
+        <iframe width="250" height="180" src="https://www.youtube.com/embed/n_LKGnx9OMc" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+        <iframe width="250" height="180"src="https://www.youtube.com/embed/E937mlU2kd4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe> 
+    
+</div>
+<!----------------------------------------->  
+                    
+    
+    
+                    
+<!-----------------Congresos-------------------->
+<div role="tabpanel"  class="tab-pane fade" id="Congresos">
+    <h1>Congresos</h1>                   
+        <iframe width="250" height="180" src="https://www.youtube.com/embed/n_LKGnx9OMc" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+        <iframe width="250" height="180"src="https://www.youtube.com/embed/E937mlU2kd4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe> 
+
+</div>
+<!----------------------------------------->  
+                    
+    
+    
+<!-----------------Simposios-------------------->
+<div role="tabpanel"  class="tab-pane fade" id="Simposios">
+    <h1>Simposios</h1>                   
+        <iframe width="250" height="180" src="https://www.youtube.com/embed/n_LKGnx9OMc" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+        <iframe width="250" height="180"src="https://www.youtube.com/embed/E937mlU2kd4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe> 
+</div>
+<!----------------------------------------->
+                    
+            
+    
+    
+<!-----------------Cineclub-------------------->
+<div role="tabpanel"  class="tab-pane fade" id="Cineclub">
+    <h1>Cineclub</h1>                   
+        <iframe width="250" height="180" src="https://www.youtube.com/embed/n_LKGnx9OMc" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+        <iframe width="250" height="180"src="https://www.youtube.com/embed/E937mlU2kd4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe> 
+</div>
+<!-----------------------------------------> 
+    
+    
+
+<!-----------------Festivales-------------------->
+<div role="tabpanel"  class="tab-pane fade" id="Festivales">
+    <h1>Festivales</h1>                   
+        <iframe width="250" height="180" src="https://www.youtube.com/embed/n_LKGnx9OMc" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+        <iframe width="250" height="180"src="https://www.youtube.com/embed/E937mlU2kd4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe> 
+</div>
+<!----------------------------------------->  
+
+    
+    
+                    
+<!-----------------Otros-------------------->
+<div role="tabpanel"  class="tab-pane fade" id="Otros">
+    <h1>Otros</h1>                   
+        <iframe width="250" height="180" src="https://www.youtube.com/embed/n_LKGnx9OMc" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+        <iframe width="250" height="180"src="https://www.youtube.com/embed/E937mlU2kd4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe> 
+</div>
+<!----------------------------------------->  
+                    
+                    
+                    
+                    
+</div>
+</aside>
+</section>
+</div>
+        
+        
+<!-------------CONTACTANOS------------------>
+    <footer class="wrapper">
+        <div id="formulario-contacto" class="row">
+            <h5 class="condensed titulo-frm">Contáctanos</h5>
+            <form action="mail/send-mail.php" method="post" class="col s12">
+                <div class="row">
+                    <div class="input-field col s6">
+                        <i class="icon-user prefix"></i>
+                        <input id="nombre" name="nombre" type="text" class="validate">
+                        <label for="nombre" class="condensed">Nombre</label>
+                    </div>
+                    <div class="input-field col s6">
+                        <i class="icon-mail prefix"></i>
+                        <input id="correo" name="correo" type="text" class="validate">
+                        <label for="correo" class="condensed">Correo</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s8">
+                        <i class="icon-message prefix"></i>
+                        <input id="mensaje" name="mensaje" type="text" class="validate">
+                        <label for="mensaje" class="condensed">Mensaje</label>
+                    </div>
+                    <div class="input-field col s4">
+                        <button class="btn waves-effect waves-light condensed" type="submit" name="action">Enviar
+                            <i class="icon-send right"></i>
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div id="contacto">
+            <h6 class="titulo-frm">Contacto</h6>
+            <div id="datos">
+                <p><i class="icon-location"></i> <strong>Dirección: </strong>Av. Universidad #1001, Col Chamilpa, Cuernavaca, Morelos. C.P. 62209</p>
+                <p><i class="icon-phone"></i> <strong>Teléfono: </strong> 329-7096 | Cómputo: ext. 2035</p>
+                <p><i class="icon-at"></i> <strong>Correo:</strong> facartes@uaem.mx | extension.fa@uaem.mx</p>
+            </div>
+            <h6 class="titulo-frm">Redes Sociales</h6>
+            <div id="redes-sociales">
+                <a href="https://www.facebook.com/FacultadArtes" class="facebook" target="_blank"><i class="icon-facebook"></i></a>
+                <a href="https://twitter.com/facultadartes/media" class="twitter" target="_blank"><i class="icon-twitter"></i></a>
+                <a href="https://www.youtube.com/channel/UCZtq8nJCBfwR1lzHT5BzVkw/videos?view=0&sort=dd&shelf_id=0" class="youtube" target="_blank"><i class="icon-youtube"></i></a>
+            </div>
+        </div>
+        <div id="mapa-artes">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15091.26887843614!2d-99.23923!3d18.983678!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x389970cfa2edc8d0!2sFacultad+de+Artes+UAEM!5e0!3m2!1ses!2smx!4v1490160948477" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+        <div id="copyright">
+            <h6 class="light">Universidad Autónoma del Estado de Morelos</h6>
+            <h6 class="light">Copyright &copy; 2017</h6>
+        </div>
+    </footer>
+    
+    
+    
+    
+    
+    
+    <!---------------Js Script-------------->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>   
+    
+</body>
+</html>
